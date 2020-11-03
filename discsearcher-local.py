4e2b8158e3fa1cd0a35595677ab9988a37b24185
ScriptJunky <disposable.info@null.com>
2020-11-03 00:30:12 -0500

    Removed unecessary comment.

--- discsearcher-local.py
+++ discsearcher-local.py
@@ -6,7 +6,6 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', None)
 pd.set_option('display.expand_frame_repr', False)
-#pd.set_option('display.colheader_justify', 'left')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--mfgr', help='Manufacturer of the disc')

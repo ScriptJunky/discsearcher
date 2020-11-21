a1830d3c91c6f3ea186e6db6d70c79c35a99a334
ScriptJunky <disposable.info@null.com>
2020-11-21 16:37:20 -0500

    Removed finalfilter print.

--- discsearcher-local.py
+++ discsearcher-local.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import pandas as pd
 import argparse
@@ -8,19 +9,26 @@ pd.set_option('display.max_colwidth', None)
 pd.set_option('display.expand_frame_repr', False)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--mfgr', help='Manufacturer of the disc')
-parser.add_argument('--name', help='Disc model or name')
-parser.add_argument('--type', help='Type of disc')
-parser.add_argument('--speed', help='Speed rating')
-parser.add_argument('--glide', help='Glide')
-parser.add_argument('--turn', help='Turn (prefix with negative supported)')
-parser.add_argument('--fade', help='Fade')
+parser.add_argument('--mfgr', action='append', help='Manufacturer of the disc')
+parser.add_argument('--name', action='append', help='Disc model or name')
+parser.add_argument('--type', action='append', help='Type of disc')
+parser.add_argument('--speed', action='append', help='Speed rating')
+parser.add_argument('--glide', action='append', help='Glide')
+parser.add_argument('--turn', action='append', help='Turn (prefix with negative supported)')
+parser.add_argument('--fade', action='append', help='Fade')
 parser.add_argument('--full', action='store_true', help='Print full contents of CSV file')
 args = parser.parse_args()
 
 csv = pd.read_csv('discs.csv', header=0, delimiter=',')
 
-filters = []
+namefilters = []
+typefilters = []
+speedfilters = []
+glidefilters = []
+turnfilters = []
+fadefilters = []
+
+finalfilter = []
 
 if args.full:
     print(csv)
@@ -30,28 +38,40 @@ if len(sys.argv) < 2:
     parser.print_help(sys.stderr)
     sys.exit(1)
 
-if args.mfgr: 
-    filters.append('(csv.Manufacturer == ' + "'" + args.mfgr + "'" + ')')
+if args.mfgr:
+    mfgrfilters = f'csv.Manufacturer.isin({args.mfgr})'
+    finalfilter.append(mfgrfilters)
 
 if args.name:
-    filters.append('(csv.Name == ' + "'" + args.name + "'" + ')')
+    namefilters = f'csv.Name.isin({args.name})'
+    finalfilter.append(namefilters)
 
 if args.type:
-    filters.append('(csv.Type == ' + "'" + args.type + "'" + ')')
+    typefilters = f'csv.Type.isin({args.type})'
+    finalfilter.append(typefilters)
 
 if args.speed:
-    filters.append('(csv.Speed == ' + args.speed + ')')
+    speedfilters = f'csv.Speed.isin({args.speed})'
+    speedfilters = re.sub(r"'", r"", speedfilters)
+    finalfilter.append(speedfilters)
 
 if args.glide:
-    filters.append('(csv.Glide == ' + args.glide + ')')
+    glidefilters = f'csv.Glide.isin({args.glide})'
+    glidefilters = re.sub(r"'", r"", glidefilters)
+    finalfilter.append(glidefilters)
 
 if args.turn:
-    filters.append('(csv.Turn == ' + args.turn + ')')
+    turnfilters = f'csv.Turn.isin({args.turn})'
+    turnfilters = re.sub(r"'", r"", turnfilters)
+    finalfilter.append(turnfilters)
 
 if args.fade:
-    filters.append('(csv.Fade == ' + args.fade + ')')
+    fadefilters = f'csv.Fade.isin({args.fade})'
+    fadefilters = re.sub(r"'", r"", fadefilters)
+    finalfilter.append(fadefilters)
 
-finalargs = " & ".join(filters)
-newcsv = 'print(csv[' + finalargs + '])'
+finalfilter = ' & '.join(finalfilter)
+
+newcsv = 'print(csv[' + finalfilter + '])'
 
 exec(newcsv)

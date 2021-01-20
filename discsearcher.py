ebc8539dda558197d1770322628e9f9a3b5b859f
ScriptJunky <disposable.info@null.com>
2021-01-20 05:38:53 -0500

    Updated version number.

--- discsearcher.py
+++ discsearcher.py
@@ -23,7 +23,7 @@ socket.setdefaulttimeout(300)
 
 # Version Information
 contact = 'discsearcher@icloud.com'
-vnum = 2021.019
+vnum = 2021.020
 version = f'''
 discsearcher v{vnum}
 \u00A9 2020 - Throw The Roller, LLC.
